a3d8c0be95558336e73f29da3d91cca752d0993c
Neeraj Sharma <neerajs@uchicago.edu>
2020-04-10 23:50:14 -0700

    got nyt data, json from il dph, demographic data and hospital data. have no clue what is wrong with headers for hospital data.

--- Homework 1/dataset_creat.R
+++ Homework 1/dataset_creat.R
@@ -5,41 +5,124 @@ library(modelr)
 library(broom)
 library(here)
 library(lubridate)
+library(jsonlite)
+library(readxl)
+library(here)
+library(stringr)
+library(textclean)
+
+
 
 # For security reasons, my personal API key is hidden. Permission to access Census/ACS data
 # to reproduce my results can be granted here: https://api.census.gov/data/key_signup.html
 library(tidycensus)
 
-# Pulls in the most recent version of COVID 19 cases and deaths from CSSE github repo.
-# Static (April 9) versions of both of these datasets  are saved in "Raw Data." 
-cases_raw <- read_csv(curl("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"))
-deaths_raw <- read_csv(curl("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"))
+NYT <-read_csv(curl("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"))
+
+dph_json <-read_lines(curl("https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json"))
+dph_formatted <- dph_json %>% fromJSON()
+dph <- dph_formatted[[5]][[1]] %>%
+  unnest(values) %>%
+  # Note that the IL Department of Public Health counts Chicago as distinct from Cook County
+  filter(County != "Illinois") %>%
+  mutate(testDate = as.Date(testDate, "%m/%d/%y")) %>%
+  rename(date = testDate, county = County, tested = total_tested, cases = confirmed_cases) %>%
+  select(-lat,-lon) %>%
+  mutate(county = if_else(county == "Chicago", "Cook", county)) %>%
+  mutate(county = if_else(county == "Suburban Cook", "Cook", county)) %>%
+  group_by(date, county) %>%
+  summarise_each(funs(sum))# %>%
+ # pivot_longer(c(cases, deaths), names_to = "disease status", values_to = "count")
 
 # Create tidy DFs of COVID data for IL only
-cases_counties <- cases_raw %>%
-  filter(Province_State == "Illinois") %>%
-  select(-UID, -iso2, -iso3, -code3, -FIPS, -Lat, -Long_, -Combined_Key) %>%
-  pivot_longer(c(-Admin2,  -Province_State, -Country_Region), names_to = "date", values_to = "cases") %>%
-  mutate(date = as.Date(date, "%m/%d/%y"))
+NYT_counties <- NYT %>%
+  filter(state == "Illinois") %>%
+  select(-fips,-state) %>%
+  filter(date < "2020-03-17")
 
-deaths_counties <- deaths_raw %>%
-  filter(Province_State == "Illinois") %>%
-  select(-UID, -iso2, -iso3, -code3, -FIPS, -Combined_Key, -Lat, -Long_) %>%
-  pivot_longer(c(-Admin2, -Province_State, -Country_Region, -Population), names_to = "date", values_to = "deaths") %>%
-  mutate(date = as.Date(date, "%m/%d/%y"))
 
-# Summarizes data to reflect statewide trends
-cases_IL<- cases_counties %>%
-  group_by(date, Admin2, Province_State, Country_Region) %>%
-  summarize(cases = sum(cases))
-
-deaths_IL <- deaths_counties %>%
-  group_by(date, Admin2, Province_State, Country_Region, Population) %>%
-  summarize(deaths = sum(deaths))
-
-# Group together cases and deaths
-cases_deaths_IL <- left_join(cases_IL, deaths_IL) %>%
-  pivot_longer(c(cases, deaths), names_to = "disease status", values_to = "count") %>%
-  mutate(percapita = count/Population) %>%
+covid <- full_join(NYT_counties, dph) %>%
+  arrange(-desc(date)) %>%
+  group_by(county) %>%
+  arrange(-desc(county)) %>%
   mutate(days_since_first_case = date - ymd("2020-01-24")) %>%
   mutate(days_since_sheltering = date - mdy("3/20/2020"))
+
+demos_full <- get_acs(geography = "county", 
+                               variables = c(`population-` = "B01003_001",
+                                             `median_income-` = "B19013_001",
+                                             `num_hh-` = "B10063_001",
+                                             `num_hh_w_people_under18-` = "B11005_002",
+                                             `num_hh_w_people_over60-` = "B11006_002",
+                                             num_owneroccupants_morethan1_per_room = "B25014_005",
+                                             num_owneroccupants_morethan15_per_room = "B25014_006",
+                                             num_owneroccupants_morethan2_per_room = "B25014_007",
+                                             num_renteroccupants_morethan1_per_room = "B25014_011",
+                                             num_renteroccupants_morethan15_per_room = "B25014_012",
+                                             num_renteroccupants_morethan2_per_room = "B25014_013",
+                                             `pop_under18-` = "B09001_001",
+                                             `enrolled_in_school-` = "B14001_002"), year = 2018, state = "Illinois", output = "wide") %>%
+  rowwise() %>%
+  mutate(`num_hh_morethan1_person_perroom-E` = sum(num_owneroccupants_morethan1_per_roomE + 
+                                                     num_owneroccupants_morethan15_per_roomE + 
+                                                     num_owneroccupants_morethan2_per_roomE + 
+                                                     num_renteroccupants_morethan1_per_roomE + 
+                                                     num_renteroccupants_morethan15_per_roomE + 
+                                                     num_renteroccupants_morethan2_per_roomE)) %>%
+  # aggregating error by square rooting the sum of squared errors
+  mutate(`num_hh_morethan1_person_perroom-M` = sqrt(sum(num_owneroccupants_morethan1_per_roomM ^ 2 + num_owneroccupants_morethan15_per_roomM ^
+                                                          2 + num_owneroccupants_morethan2_per_roomM ^ 2 + num_renteroccupants_morethan1_per_roomM ^
+                                                          2 + num_renteroccupants_morethan15_per_roomM ^ 2 + num_renteroccupants_morethan2_per_roomM ^
+                                                          2))) %>%
+  select(-GEOID,
+         -num_owneroccupants_morethan1_per_roomE,
+         -num_owneroccupants_morethan15_per_roomE,
+         -num_owneroccupants_morethan2_per_roomE,
+         -num_renteroccupants_morethan1_per_roomE,
+         -num_renteroccupants_morethan15_per_roomE,
+         -num_renteroccupants_morethan2_per_roomE,
+         -num_owneroccupants_morethan1_per_roomM,
+         -num_owneroccupants_morethan15_per_roomM,
+         -num_owneroccupants_morethan2_per_roomM,
+         -num_renteroccupants_morethan1_per_roomM,
+         -num_renteroccupants_morethan15_per_roomM,
+         -num_renteroccupants_morethan2_per_roomM) %>%
+  separate(NAME, into = c("Admin2", NA), sep = " County, Illinois")
+
+demos <- demos_full %>%
+  select(-(ends_with("-M"))) %>%
+  rename(county = Admin2) %>%
+  arrange(-desc(county))
+
+covid_demos <- left_join(dph, demos)
+
+# I grab hospital data from Illinois Health Facilities and Services Review Board
+# https://www2.illinois.gov/sites/hfsrb/InventoriesData/FacilityProfiles/Pages/default.aspx
+
+hosp <- read_excel(path = "Homework 1/Raw Data/2018 AHQ Data File.xls" , col_names = FALSE, sheet = "Hospital Utilization Data")
+
+hosp <- hosp[-4, ]
+hosp[1,8:18] <- paste0("beds", subset(hosp, select = c(8:18))[2,])
+hosp[1,19:24] <- paste0("MedicalSurgicalAdmissions", subset(hosp, select = c(19:24))[2,])
+hosp[1,25:30] <- paste0("PatientDaysOfCare", subset(hosp, select = c(25:30))[2,])
+hosp[1,84:89] <- paste0("AdolescentMentalIllness", subset(hosp, select = c(84:89))[2,])
+hosp[1,90:95] <- paste0("AdultMentalIllness", subset(hosp, select = c(90:95))[2,])
+hosp <- hosp[-(2:3), ]
+
+colnames(hosp) <- hosp[1,]
+hosp <- hosp[-1, ]
+
+hosp_full <- hosp %>% 
+  select(Hospital, county = County) %>%
+  select(`bedsMedical- Surgical`)
+         #, `beds-Intensive Care`, `beds-Pediatric`, `beds-Obstetrics/Gynecology`, `beds-Long-Term Care`, `beds-Neonatal ICU`,
+ #                                       `beds-Rehabilitation`, `beds-Acute Mental Illness`, `beds-Long-Term Acute Care`, `Direct Admissions to Intensive Care`, `Transfers into  Intensive Care`, 
+ #                                       `Patient Days Direct Admissions to Intensive Care`, `Patient Days Transfers to Intensive Care`, `Intensive Care Peak Beds Set Up`)
+         
+         
+
+
+
+
+
+
